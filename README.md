304790c5f6608785db7e2c0f50defc49464b8de4
马坤坤 <makunkun163@163.com>
2021-02-27 18:12:13 +0800

    feat: computed 、 reactive

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 ## 1.vue3 + vite 搭建项目
 
+* npm init @vitejs/app vue3-use-demo --template vue
+
+* npm install
+
+* npm run dev
+
 ## 2.vue3的使用
+
+* Composition API
+
+* setup & ref
+
+* Methods
+
+* Computed Properties
+
+* Reactive Syntax
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,55 @@
 <template>
   <div>
+    <h2>computed</h2>
+    <p>Space Left: {{ spaceList }} out of {{ capacity }}</p>
+
     <p>Capacity: {{ capacity }}</p>
     <button @click="increaseCapacity">Increase Capacity</button>
+
+    <h2>attending</h2>
+    <ul>
+      <li v-for="(name, index) in attending" :key="index">
+        {{name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed, reactive, toRefs } from 'vue';
 export default {
   setup() {
-    const capacity = ref(3);
+    // // ref
+    // const capacity = ref(3);
+    // const attending = ref(['Tim', 'Bob', 'Joe'])
 
+    // // methods; ref => getter setter
+    // function increaseCapacity() {
+    //   return capacity.value++;
+    // }
+
+    // // computed
+    // const spaceList = computed(() => {
+    //   return capacity.value - attending.value.length;
+    // });
+
+    // // return
+    // return { capacity, increaseCapacity, attending, spaceList };
+
+    // reactive
+    const event = reactive({
+      capacity: 3,
+      attending: ['Tim', 'Bob', 'Joe'],
+      spaceList: computed(() => {
+        return event.capacity - event.attending.length;
+      })
+    });
+    // methods; reactive => getter setter
     function increaseCapacity() {
-      return capacity.value++;
+      return event.capacity++;
     }
-
-    return { capacity,increaseCapacity };
+    // toRefs (遍历所有event属性, 如果不是 extends ref, 则ref它)
+    return { ...toRefs(event), increaseCapacity }
   }
 }
 </script>
